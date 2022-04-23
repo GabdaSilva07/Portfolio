ae83fb3edcb19f4fe86fb6bd00d51b1e12087477
Gabriel da Silva <gabdasilva96@gmail.com>
2022-04-23 17:42:13 +0100

    Created btn, Just need to add the styles.

---
+++ portfolio/src/Components/Button/Button.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Button.css";
+
+//List of button styles
+const STYLES = [
+  "Primary-Style",
+  "Secondary-Style",
+  "Primary-Outline",
+  "Secondary-Outline",
+];
+const SIZE = ["Small", "Medium", "Large", "Phone"];
+const COLOR = ["Primary", "Secondary"];
+
+interface IButton {
+  style: string;
+  size: string;
+  color: string;
+  children: any;
+  onClick: any;
+}
+
+export function Button({ children, style, size, color, onClick }: IButton) {
+  //Check if the button style
+  let checkButtonStyle: (style: string) => string = (style) => {
+    if (STYLES.includes(style)) {
+      return style;
+    } else {
+      return "Primary-Style";
+    }
+  };
+  //Check if the button size
+
+  let checkButtonSize: (string: string) => string = (size) => {
+    if (SIZE.includes(size)) {
+      return size;
+    } else {
+      return "Medium";
+    }
+  };
+  //Check if the button color
+  let checkButtonColor: (string: string) => string = (color) => {
+    if (COLOR.includes(color)) {
+      return color;
+    } else {
+      return "Primary";
+    }
+  };
+
+  return (
+    <button
+      className={`btn ${checkButtonStyle(style)} ${checkButtonSize(
+        size
+      )} ${checkButtonColor(color)}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
